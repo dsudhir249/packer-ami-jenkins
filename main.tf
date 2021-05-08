3bef8ca25e90a4a85ed9a0f2274f6cc1c2721d54
dsudhir249 <76003410+dsudhir249@users.noreply.github.com>
2021-05-08 16:17:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -83,3 +83,15 @@ resource "aws_subnet" "Private-subnet" {
     }
 } 
 
+resource "aws_instance" "web-1" {
+    ami = "${var.imagename}"
+    availability_zone = "us-east-1a"
+    instance_type = "t2.nano"
+    subnet_id = "${aws_subnet.Public-subnet.id}"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    key_name = "factsetkeypair"
+    associate_public_ip_address = true
+    tags = {
+        Name = "Server-1"
+    }
+}
