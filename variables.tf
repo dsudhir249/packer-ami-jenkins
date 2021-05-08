b7fe2cf93b7346d2d90aede8ecd921a1f044c48f
dsudhir249 <76003410+dsudhir249@users.noreply.github.com>
2021-05-08 15:54:25 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key"  {}
+variable "aws_secret_key"  {}
+variable "aws_region"  {}
+variable "vpc_cidr"  {}
+variable "environment"  {}
+variable "IGW_name"  {}
+variable "vpc_name"  {}
+variable "publicsubnet" {}
+variable "Main_Routing_Table" {}
+variable "Private_Routing_Table" {}
+variable "imagename" {}
